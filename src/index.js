8e5bc1d9f6c2d20c80a768abb4d5ff0889a8d7b6
anastasia1311 <nastiadardinskaya@tut.by>
2020-10-01 21:43:49 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } 
+   else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } 
+   else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } 
+   else {
+    for( var i = 0; i < array.length; i++ ){
+     sum += array[i]; //don't forget to add the base
+ }
+ var avg = sum/array.length;
+    return avg;
+  }
 }
